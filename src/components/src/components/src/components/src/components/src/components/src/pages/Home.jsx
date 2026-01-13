4c0878f9c4dcabab3dbb840e986c2c0d00c0fdfb
mkassociates <techwebcode.com@gmail.com>
2026-01-13 19:55:41 +0500

    Create Home.jsx

---
+++ src/components/src/components/src/components/src/components/src/components/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import Hero from "../components/Hero";
+import Courses from "../components/Courses";
+import Services from "../components/Services";
+import Portfolio from "../components/Portfolio";
+import Contact from "../components/Contact";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Courses />
+      <Services />
+      <Portfolio />
+      <Contact />
+    </>
+  );
+}
