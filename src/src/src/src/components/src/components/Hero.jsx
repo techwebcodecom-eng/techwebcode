de2d34d427d1c0a16f330a9bef45d0aa7d1111bb
mkassociates <techwebcode.com@gmail.com>
2026-01-13 19:44:24 +0500

    Create Hero.jsx

---
+++ src/src/src/src/components/src/components/Hero.jsx
@@ -0,0 +1,12 @@
+export default function Hero() {
+  return (
+    <section>
+      <h1>Learn | Build | Earn with Tech Web Code</h1>
+      <p>
+        Web Development Courses, Professional Services & Real World Projects
+      </p>
+      <button>Start Learning</button>
+      <button>Hire Us</button>
+    </section>
+  );
+}
