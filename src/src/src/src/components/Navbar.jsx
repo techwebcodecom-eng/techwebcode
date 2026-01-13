76dddee4b727ca8b3dfaa6eddfc5eb621569665c
mkassociates <techwebcode.com@gmail.com>
2026-01-13 19:43:36 +0500

    Create Navbar.jsx

---
+++ src/src/src/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <h2>Tech Web Code</h2>
+      <ul>
+        <li>Home</li>
+        <li>Courses</li>
+        <li>Services</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
