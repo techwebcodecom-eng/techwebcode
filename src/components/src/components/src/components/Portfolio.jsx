b0d62b5d02530b3c4af8d302ec5e261c2e01ce62
mkassociates <techwebcode.com@gmail.com>
2026-01-13 19:53:02 +0500

    Create Portfolio.jsx

---
+++ src/components/src/components/src/components/Portfolio.jsx
@@ -0,0 +1,17 @@
+export default function Portfolio() {
+  return (
+    <section>
+      <h2>Live Projects</h2>
+
+      <div className="card">
+        <h4>Agency Website</h4>
+        <a href="#">GitHub Repo</a> | <a href="#">Live Demo</a>
+      </div>
+
+      <div className="card">
+        <h4>Hotel Website</h4>
+        <a href="#">GitHub Repo</a> | <a href="#">Live Demo</a>
+      </div>
+    </section>
+  );
+}
