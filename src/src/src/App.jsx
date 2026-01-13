1f8918c0393543bf40f067f7525fb8fe342a530c
mkassociates <techwebcode.com@gmail.com>
2026-01-13 19:42:52 +0500

    Create App component with Navbar, Home, and Footer

---
+++ src/src/src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
+  );
+}
